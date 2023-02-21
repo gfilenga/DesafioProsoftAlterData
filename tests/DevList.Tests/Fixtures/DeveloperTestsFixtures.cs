19097a206c256bba91b15f8919d199851b3fe040
Guilherme Rodrigues Filenga <guilherme.filenga@gmail.com>
2023-02-21 10:32:27 -0300

    feat: fixture

---
+++ tests/DevList.Tests/Fixtures/DeveloperTestsFixtures.cs
@@ -0,0 +1,34 @@
+﻿using DevList.Domain.Models;
+using DnsClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DevList.Tests.Fixtures
+{
+    [CollectionDefinition(nameof(DeveloperCollection))]
+    public class DeveloperCollection : ICollectionFixture<DeveloperTestsFixtures>
+    { }
+    public class DeveloperTestsFixtures : IDisposable
+    {
+        public Developer GenerateValidDeveloper()
+        {
+            return new Developer()
+            {
+                Avatar = "https:cloudflare.com.br/fake.jpg",
+                Email = "guilherme.filenga@prosoft.com.br",
+                Linguagem = "c#",
+                Login = "guilha22",
+                Name = "Guilherme Filenga",
+                Squad = "onboarding"
+            };
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
--- tests/DevList.Tests/Models/DeveloperTests.cs
+++ tests/DevList.Tests/Models/DeveloperTests.cs
@@ -1,23 +1,24 @@
 using DevList.Domain.Models;
+using DevList.Tests.Fixtures;
 using FluentAssertions;
 
 namespace DevList.Tests.Models
 {
+    [Collection(nameof(DeveloperCollection))]
     public class DeveloperTests
     {
+        private readonly DeveloperTestsFixtures _devFixture;
+
+        public DeveloperTests(DeveloperTestsFixtures devFixture)
+        {
+            _devFixture = devFixture;
+        }
+
         [Fact]
         public void Developer_UpdateModel_ShouldUpdateTimerField()
         {
             // Arrange
-            var developer = new Developer()
-            {
-                Avatar = "https:cloudflare.com.br/fake.jpg",
-                Email = "guilherme.filenga@prosoft.com.br",
-                Linguagem = "c#",
-                Login = "guilha22",
-                Name = "Guilherme Filenga",
-                Squad = "onboarding"
-            };
+            var developer = _devFixture.GenerateValidDeveloper();
 
             // Act
             developer.UpdateModel();
